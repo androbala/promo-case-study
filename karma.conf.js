28aba8fd9bcb50406e2dd4f0ea2fc04e813eb86c
androbala <androbala@gmail.com>
2016-11-24 08:31:38 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -18,12 +18,11 @@ module.exports = function(config) {
 
     frameworks: ['jasmine'],
 
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
 
     plugins: [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
-      'karma-phantomjs-launcher',
       'karma-jasmine',
       'karma-junit-reporter'
     ],
