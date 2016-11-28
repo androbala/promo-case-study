04dee9da209a76b7e5c73b90fa11765752493b79
androbala <androbala@gmail.com>
2016-11-28 01:47:30 -0600

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,8 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-slick/dist/slick.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'app.js',
       'modules/product_details/prdt_detl_module.js',
       'modules/services/*.js',
